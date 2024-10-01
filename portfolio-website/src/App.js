619c56ec646bde1ab72d0815ba859784088b9718
io6-ankit <156927710+io6-ankit@users.noreply.github.com>
2024-10-01 17:12:44 +0530

    code commit

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -4,13 +4,18 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Skills from "./components/Skills";
+import { Route, Router } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
-      {/* <Home /> */}
-      <Skills />
+      <Router>
+        <Route exact path="/" element={<Navbar />} />
+
+        {/* <Home /> */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/skill" element={<Skills />} />
+      </Router>
     </>
   );
 }
