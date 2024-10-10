9ab8ca4beaa63bacc2976836667f038ca047430f
io6-ankit <156927710+io6-ankit@users.noreply.github.com>
2024-10-10 15:08:41 +0530

    portfolio

--- portfolio-website/src/components/Experience.jsx
+++ portfolio-website/src/components/Experience.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import "./experience.css";
+import { Typography } from "@mui/material";
+import ExperienceImage from "./assets/image/experience-image.png";
 const Experience = () => {
   return (
-    <Box className="container-skills">
-      <h1 className="skill-tytle">Skills</h1>
-      <Box className="skills">
-        <Box className="skillsType">vfvfs</Box>
-        <Box className="skillsType">fvfsv</Box>
-        <Box className="skillsType">fvsfvfs</Box>
+    <Box className="Experience-main-section">
+      <Box className="Experience-main-image-section">
+        <img
+          className="Experience-image-section"
+          src={ExperienceImage}
+          alt="experience-image"
+        />
       </Box>
-      <Box className="skills-2">
-        <Box className="skillsType">fvfsvs</Box>
-        <Box className="skillsType">fsvfs</Box>
-        <Box className="skillsType">fsgvfsvs</Box>
+      <Box className="Experience-parent-section">
+        <Box className="Experience-main-heading-section">
+          <Typography variant="h3">Experience</Typography>
+        </Box>
+        <Box className="Experience-heading-section">
+          <Typography variant="h4">Fresher</Typography>
+        </Box>
+        <Box className="Experience-paragraph-section">
+          <Typography variant="p">
+            I am a fresher.But I have basic knowledge of HTML, CSS, ReactJS,
+            JavaScript, Bootstrap, Material UI, Parcel, Git, Github, Netlify. I
+            have completed many projects using HTML, CSS, React JS, JavaScript,
+            Material UI, Parcel, Bootstrap.
+          </Typography>
+        </Box>
       </Box>
-      <Box>fvfss</Box>
     </Box>
   );
 };
